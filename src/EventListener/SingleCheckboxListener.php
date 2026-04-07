6340e84fe1b60d01d28a25d04c97a886d9d39f01
Thomas Körner <t.koerner@heimrich-hannot.de>
2026-04-07 21:42:52 +0200

    make checkbox submit working

--- src/EventListener/SingleCheckboxListener.php
+++ src/EventListener/SingleCheckboxListener.php
@@ -22,6 +22,8 @@ class SingleCheckboxListener
         }
 
         $GLOBALS['TL_DCA']['tl_form_field']['fields']['text']['eval']['rte'] = 'tinyMCE_option';
+        $GLOBALS['TL_DCA']['tl_form_field']['fields']['text']['eval']['tl_class'] = 'clr';
+        $GLOBALS['TL_DCA']['tl_form_field']['fields']['text']['eval']['mandatory'] = true;
         $GLOBALS['TL_DCA']['tl_form_field']['fields']['value']['label'] = &$GLOBALS['TL_LANG']['tl_form_field']['singleCheckboxValue'];
     }
 }
